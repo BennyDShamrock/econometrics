c077780c97b60d36eb46109999a23310bd6a70dc
BennyDShamrock <66623824+BennyDShamrock@users.noreply.github.com>
2021-06-30 12:50:23 +0200

    Update IV_Schaetzung.R
    In line 30, there was a typo (exper^1) instead of (exper^2)

--- IV_Schaetzung.R
+++ IV_Schaetzung.R
@@ -27,7 +27,7 @@ lm(data = oursample, educ ~ fatheduc)
 # Modelle aufsetzen
 stage1 <- lm(educ ~ exper + I(exper^2) + motheduc + fatheduc, data = oursample)
 man.2SLS <- lm(log(wage) ~ fitted(stage1) + exper + I(exper^2), data = oursample)
-aut.2SLS <- ivreg(log(wage) ~ educ + exper + I(exper^1) | motheduc + fatheduc + exper + I(exper^2), data = oursample)
+aut.2SLS <- ivreg(log(wage) ~ educ + exper + I(exper^2) | motheduc + fatheduc + exper + I(exper^2), data = oursample)
 
 # Modelle vergleichen
 stargazer(stage1, man.2SLS, aut.2SLS, type="text", keep.stat=c("n", "rsq"))
